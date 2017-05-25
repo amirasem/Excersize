662abc11756b86529cff29de1002fb8cf2242d8f
amirasem <amiryousefi98@gmail.com>
2017-05-25 16:54:53 -0700

    Test_Q1

---
+++ Test/HammamTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class HammamTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void getMin() throws Exception {
+
+    }
+
+    @Test
+    public void getLast() throws Exception {
+
+    }
+
+    @Test
+    public void getFirst() throws Exception {
+
+    }
+
+    @Test
+    public void getLess() throws Exception {
+
+    }
+
+    @Test
+    public void getRecentlyRemoved() throws Exception {
+
+    }
+
+}
