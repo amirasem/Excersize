27b93b6f17cf74d2fe861cc469527d5a9bc02a43
amirasem <amiryousefi98@gmail.com>
2017-05-25 17:40:19 -0700

    Test_Q1

--- Test/HammamTest.java
+++ Test/HammamTest.java
@@ -8,40 +8,30 @@ import static org.junit.Assert.*;
 public class HammamTest {
     @Test
     public void add() throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        Hammam<String> customerName = new Hammam<>();
-        customerName.add(null);
+    }
+    @Test
+    public Object getMin() throws Exception {
+            return null;
     }
 
     @Test
-    public void getMin() throws Exception {
-
+    public Object getLast() throws Exception {
+            return null;
     }
 
     @Test
-    public void getLast() throws Exception {
-
+    public Object getFirst() throws Exception {
+            return null;
     }
 
     @Test
-    public void getFirst() throws Exception {
-
+    public Object[] getLess() throws Exception {
+            return null;
     }
 
     @Test
-    public void getLess() throws Exception {
-
-    }
-
-    @Test
-    public void getRecentlyRemoved() throws Exception {
-
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Hammam<String> customeNames = new Hammam<>();
-        customeNames.add(scanner.next());
+    public Object[] getRecentlyRemoved() throws Exception {
+            return null;
     }
 
 }
