824ccb1303d47a20c0ce9829ed97c14c577769d7
AmirHosein <amiryousefi98@gmail.com>
2017-06-04 12:48:35 +0430

    HW3

--- Test/HammamTest.java
+++ Test/HammamTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static org.junit.Assert.*;
@@ -8,30 +10,108 @@ import static org.junit.Assert.*;
 public class HammamTest {
     @Test
     public void add() throws Exception {
+        Hammam<Integer> numbers= new Hammam<>();
+        numbers.add(5);
+        numbers.add(15);
+        numbers.add(null);
+    }
+
+
+
+    @Test(expected = IllegalStateException.class)
+    public void getMinForNull() throws Exception {
+        Hammam<Integer> numbers= new Hammam<>();
+        numbers.add(null);
+        numbers.add(null);
+        numbers.add(null);
+        assertEquals(5 , numbers.getMin() , numbers.getMin());
+        System.out.println("the min  = " + numbers.getMin());
     }
     @Test
-    public Object getMin() throws Exception {
-            return null;
+    public void getMinForEquals() throws Exception {
+        Hammam<Integer> numbers = new Hammam<>();
+        numbers.add(15);
+        numbers.add(15);
+        numbers.add(15);
+        assertEquals(15 , numbers.getMin() , numbers.getMin());
+        System.out.println("the min  is  :" + numbers.getMin());
+    }
+    @Test
+    public void getMinForNegativeNumber() throws Exception{
+        Hammam<Integer> numbers  = new Hammam<>();
+        numbers.add(-1);
+        numbers.add(-1500);
+        numbers.add(null);
+        numbers.add(1500);
+        assertEquals(-1500 , numbers.getMin() , numbers.getMin());
+        System.out.println("the min is  :" + numbers.getMin());
+
+    }
+    @Test(expected = IllegalStateException.class)
+    public void getLastForEmpty() throws Exception {
+        Hammam <Integer> numbers  = new Hammam<>();
+        numbers.getLast(false);
+        System.out.println(numbers.getLast(false));
+    }
+    @Test
+    public void getLastForNull() throws Exception{
+        Hammam<Integer> numbers = new Hammam<>();
+        numbers.add(null);
+        numbers.add(null);
+        numbers.add(15);
+        numbers.add(null);
+        numbers.add(153);
+        numbers.getLast(false);
+
+        System.out.println("the last is "  + numbers.getLast(false));
+    }
+
+
+
+    @Test (expected = IllegalStateException.class)
+    public void getFirstForEmpty() throws Exception {
+        Hammam<Integer> numbers = new Hammam<>();
+        numbers.getFirst(false);
+    }
+    @Test
+    public void getFirstForNull() throws Exception{
+        Hammam<Integer> numbers  = new Hammam<>();
+        numbers.add(null);
+        numbers.add(15);
+        numbers.add(null);
+        numbers.getFirst(false);
+        System.out.println(numbers.getFirst(false));
     }
 
     @Test
-    public Object getLast() throws Exception {
-            return null;
+    public void getLess() throws Exception {
+        Hammam<Integer> numbers =  new Hammam<>();
+        numbers.add(15);
+        numbers.add(16);
+        numbers.add(-11);
+        numbers.add(null);
+        numbers.add(13);
+        assertArrayEquals(numbers.getLess(13 , false) , numbers.getLess(13 , false));
+        System.out.println(Arrays.toString(numbers.getLess(13, false)));
     }
 
-    @Test
-    public Object getFirst() throws Exception {
-            return null;
+
+
+    @Test(expected = NullPointerException.class)
+    public void getRecentlyRemovedForEmpty() throws Exception {
+        Hammam<Integer> numbers = new Hammam<>();
+        numbers.getRecentlyRemoved(0);
+    }
+    @Test(expected = NullPointerException.class)
+    public void getRecentlyRemovedForNegetiveN() throws Exception{
+        Hammam<Integer> numbers  = new Hammam<>();
+        numbers.add(15);
+        numbers.add(16);
+        numbers.getFirst(true);
+        numbers.getRecentlyRemoved(-1);
     }
 
-    @Test
-    public Object[] getLess() throws Exception {
-            return null;
-    }
 
-    @Test
-    public Object[] getRecentlyRemoved() throws Exception {
-            return null;
-    }
+
 
 }
--- src/Hammam.java
+++ src/Hammam.java
@@ -1,120 +1,152 @@
 import java.util.ArrayList;
 
 
- public class Hammam<T extends Comparable<T>> implements Comparable<T> {
+public class Hammam<T extends Comparable<T>> {
 
-    ArrayList<T> items = new ArrayList<T>();
-    ArrayList<T> deletedItems = new ArrayList<T>();
-    @Override
+    private ArrayList<T> items = new ArrayList<>();
+    private ArrayList<Comparable> deletedItems = new ArrayList<>();
+    //     private final Constructor<? extends T> ctor;
+    private int size;
+
+//     public Hammam(Constructor<? extends T> ctor) {
+//         this.ctor = ctor;
+//     }
+
+
+    public int getSize() {
+        return size;
+    }
+
     public int compareTo(T o) {
         //TODO : overriding compareTo method
-     return 0;
+        return 0;
     }
 
-    public void add(T item)
-    {
+    void add(T item) {
         System.out.println("Enter to add....");
-        if(!items.contains(item) && item!=null) items.add(item);
+        if (!items.contains(item) && item != null) items.add(item);
     }
 
-    public T getMin() throws IllegalStateException {
+    T getMin() throws IllegalStateException {
         T min = null;
-        if(items.isEmpty()) throw new IllegalStateException();
+        if (items.isEmpty()) throw new IllegalStateException();
         else {
             for (T item : items) {
                 if (min == null) min = item;
-                if (item.compareTo(item) == 1) min = item;
+                if (item.compareTo(min) == -1) min = item;
             }
         }
         return min;
     }
-    public T getLast (boolean remove) throws IllegalStateException {
-        T last = null;
-        if(items.isEmpty()) throw new IllegalStateException();
-        else {
-            if(remove) {
-                last = items.get(items.size()-1);
+
+    T getLast(boolean remove) throws IllegalStateException {
+
+        if (items.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        T last = items.get(items.size() - 1);
+        if (!items.isEmpty()) {
+            if (remove) {
+                last = items.get(items.size() - 1);
                 deletedItems.add(last);
-                items.remove(items.size()-1);
+                items.remove(items.size() - 1);
             }
         }
         return last;
+
     }
-    public T getFirst(boolean remove) throws IllegalStateException {
-        T first = null;
+
+    T getFirst(boolean remove) throws IllegalStateException {
         ArrayList<Integer> index = new ArrayList<>();
+
         if (items.isEmpty()) throw new IllegalStateException();
         else {
+            T first = items.get(0);
             for (T item : items) {
                 index.add(items.indexOf(item));
             }
             int min = index.get(0);
             for (Integer i : index) if (i < min) min = i;
-            if (remove){
+            if (remove) {
                 first = items.get(min);
                 deletedItems.add(first);
                 items.remove(min);
             }
+            return first;
         }
-        return first;
     }
-    public T[] getLess (T element , boolean remove){
 
-        ArrayList <T> less = new ArrayList<T>();
-        for (T item:items)
-        {
-            if(item.compareTo(element) == -1) less.add(item);
+    Comparable[] getLess(T element, boolean remove) throws NullPointerException {
+        //   ArrayList<Integer> size = new ArrayList<>();
+        Comparable[] less = new Comparable[items.size()];
+
+        for (int i = 0; i < less.length; i++) {
+            if (items.get(i).compareTo(element) == -1)
+                less[i] = items.get(i);
+            if (items.get(i) == null) throw new NullPointerException();
+
         }
-        if(remove)
-        {
-            for (T item : less)
-            {
-                deletedItems.add(item);
+        if (remove) {
+            for (Comparable les : less) {
+                deletedItems.add(les);
             }
-            for (int i = 0 ; i<less.size() ;i++)
-            {
-                items.remove(less.get(i));
+            for (Comparable les : less) {
+                items.remove(les);
             }
         }
-        return (T[]) less.toArray();
-    }
-    public T[] getRecentlyRemoved(int n)
-    {
-        ArrayList<T> removed = new ArrayList<T>();
-        for (int i =deletedItems.size()-1 ; i>n ;i--)
-        {
-            removed.add(deletedItems.get(i));
-        }
-        T[] remove2 = (T[])removed.toArray();
-        for (int i = 0  ; i<removed.size() ;  i++)
-        {
-            remove2[i] = removed.get(i);
-        }
-        return remove2;
+        return less;
     }
 
-     public ArrayList<T> getItems() {
-         return items;
-     }
+    Comparable[] getRecentlyRemoved(int n) throws NullPointerException {
+        if (items.isEmpty() || n < 0) throw new NullPointerException();//n=2
+        Comparable[] removed = new Comparable[deletedItems.size()];
+        Comparable[] all = new Comparable[deletedItems.size()];
+        // T[] remove2 = null ;
+        if (n > deletedItems.size()) {
+            System.out.println("Enter one-----");
+            System.out.println("DELETED ITEMS SIZE  : " + deletedItems.size());
+            for (int i = 0; i <= deletedItems.size() - 1; i++) {
+                all[i] = deletedItems.get(i);
+                System.out.println("this is i " + i);
+                System.out.println("this is all " + all[i] + "\n" + "this is deleted items : " + deletedItems.get(i));
+            }
+            return all;
+        } else {
+            System.out.println("Enter two----");
+            System.out.println("DELETED ITEMS SIZE IS : " + deletedItems.size());
+            for (int i = 0; i <= deletedItems.size() - 1; i++) {
+                removed[i] = deletedItems.get(i);
+            }
+            reverseArray(removed);
+            Comparable[] finalResult = new Comparable[removed.length];
+            for (int i = 0; i <= n; i++) {
+                finalResult[i] = removed[n];
+            }
+            return finalResult;
+        }
+    }
 
-     public ArrayList<T> getDeletedItems() {
-         return deletedItems;
-     }
+    //or (int i = 0  ; i<removed.size() ;  i++)
+//        {
+//            assert false;
+//            remove2[i] = removed.get(i);
+//        }
+    private static void reverseArray(Comparable[] data) {
 
-     public static void main(String[] args) {
-         Hammam<Integer> hammam = new Hammam<>();
-         hammam.add(12345);
-         hammam.add(0000);
-         hammam.add(1111);
-         ArrayList<Integer> numbers = new ArrayList<>();
-         numbers = hammam.getItems();
-         Integer p = hammam.getLast(true);
-         ArrayList<Integer> integers = new ArrayList<>();
-         Integer [] integers1 = new Integer[5];
-         integers1 = hammam.getRecentlyRemoved(0);
-         //int out  = hammam.getFirst(false);
-         System.out.println(numbers + "...................." + p + "\n "  + integers1);
-     }
+        for (int i = 0; i < data.length / 2; i++) {
+            for (int j = data.length - 1; j > 0; j--) {
+                Comparable temp = data[i];
 
+                data[i] = data[j];
+
+                data[j] = temp;
+
+            }
+        }
+    }
 
 }
+
+
+
